e2cbbde199d66916ea00c8efd00038b6facaf3f0
Nicemol Johns <nicemol002@gmail.com>
2023-07-20 22:35:04 +0530

    Hosting

---
+++ app.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose')
+const express = require('express');                                           
+const app = express();  
+
+require("dotenv").config();                                                   
+   
+
+const morgan = require('morgan');
+app.use(morgan('dev'));
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+const cors = require('cors');
+app.use(cors());
+                                                 
+const PORT = 3000; 
+
+const db = require('./db/index');
+const api=require('./routers/router');
+app.use('/api',api);
+
+const path = require('path');
+app.use(express.static('./dist/frontend'));
+
+ 
+app.use(express.static(path.join(__dirname,'frontend')))
+
+app.get('*', async(req, res)=> {
+        res.sendFile(path.join(__dirname ,'angular','index.html'))});
+
+app.listen(PORT, () => {
+        console.log(`Server is running on ${PORT}`);
+})
